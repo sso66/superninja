5f1de54fadcc27f5c9fc408cac11627fbe49734b
Stephen <sun.stephen.oo@gmail.com>
2024-03-30 21:40:42 -0400

    add javascript for the about page

---
+++ about.js
@@ -0,0 +1,7 @@
+// find the elements
+const guessButton = document.getElementById('guess-button');
+guessButton.addEventListener('click', processGuess);
+
+// Start the game
+
+// Supporting functions
